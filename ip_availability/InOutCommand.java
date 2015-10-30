dad98ba7195c4f0e8e1f74054b4a23e0a59d1b20
kamena <kamena.dacheva@gmail.com>
2015-10-30 17:17:00 +0200

    Program from class | ip_availability | Kamena

---
+++ ip_availability/InOutCommand.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+import java.util.*;
+import java.text.*;
+
+public class InOutCommand {
+	static int i = 0;
+	final static HashMap<String, ArrayList<String>> hashmap = 
+	        new HashMap<String, ArrayList<String>>();
+	
+	public static void main(String[] args) {
+		final Scanner in = new Scanner(System.in);
+		
+		while(i != 3) {
+			System.out.print("Enter command: ");
+			
+			final String command = in.next();			
+			final String result = execute(command);
+
+			System.out.println(result);	
+		}
+		in.close();
+	}
+
+	private static String execute(String command) throws IllegalArgumentException {
+		final String[] split = command.split(":");	  
+		ArrayList<String> arraylist = new ArrayList<String>();
+		if ("login".equals(split[1])){
+		    if (hashmap.get(split[0]) == null) {
+		    	arraylist = new ArrayList<String>();
+			    arraylist.add("1");
+			    hashmap.put(split[0], arraylist);
+		    } else {
+		    	arraylist.add("1");
+			    hashmap.put(split[0], arraylist);
+		    }
+		    return "ok";
+		} else
+		if ("logout".equals(split[1])){	
+			arraylist = hashmap.get(split[0]);
+
+			if (hashmap.get(split[0]) == null) {
+				return "error:notlogged";
+			} else if (arraylist.get(arraylist.size()-1) == "1") {
+			    arraylist.add("0");
+			    hashmap.put(split[0], arraylist);
+			    return "ok";
+			} return "error:notlogged";
+
+		} else
+		
+		if("info".equals(split[1])) {
+			Object value = hashmap.get(split[2]);
+			if (value != null) {
+				arraylist = hashmap.get(split[2]);
+//		        System.out.println(arraylist);
+		        if (arraylist.get(arraylist.size()-1) == "1") {
+		        	return "ok:" + split[2] + ":true:" + ((arraylist.size()+1)/2);
+		        } else return "ok:" + split[2] + ":false:" + ((arraylist.size())/2);
+			} else return "error:notlogged";
+		} else
+		
+		if ("listavailable".equals(split[1])){
+			for (Object key : hashmap.keySet()) {
+				arraylist = hashmap.get(key);
+				if (arraylist.get(arraylist.size()-1) == "1") {
+					System.out.println(key);
+				}
+			}
+		} else 
+		
+		if ("shutdown".equals(split[1])) {
+			i = 3;
+		} else return "error:unknowncommand";
+		return "ok";
+	}
+}

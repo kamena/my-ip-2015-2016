87c7ce2c2fb2700ba631e9aa44ed51a85ef0e832
kamena <kamena.dacheva@gmail.com>
2015-11-17 01:22:41 +0200

    Different function for every command

--- ip_availability/src/ip_availability/CommandHandler.java
+++ ip_availability/src/ip_availability/CommandHandler.java
@@ -22,74 +22,90 @@ public class CommandHandler {
 		return split;
 	}
 	
+	private String login(String[] split, ArrayList<String> arraylist, Date dateNow, SimpleDateFormat dateFormat) {
+	    if (hashmap.get(split[0]) == null) {
+	    	arraylist = new ArrayList<String>();
+		    arraylist.add(dateFormat.format(dateNow));
+		    hashmap.put(split[0], arraylist);
+	    } else {
+			arraylist = hashmap.get(split[0]);
+
+	    	arraylist.add(dateFormat.format(dateNow));
+		    hashmap.put(split[0], arraylist);
+	    }
+	    return "ok";
+	}
+	
+	private String logout(String[] split, ArrayList<String> arraylist, Date dateNow, SimpleDateFormat dateFormat) {
+		arraylist = hashmap.get(split[0]);
+
+		if (hashmap.get(split[0]) == null) {
+			return "error:notlogged";
+		} else if (arraylist.size() % 2 != 0) {
+		    arraylist.add(dateFormat.format(dateNow));
+		    hashmap.put(split[0], arraylist);
+		    return "ok";
+		} return "error:notlogged";
+	}
+	
+	private String info(String[] split, ArrayList<String> arraylist) {	
+		Object value = hashmap.get(split[2]);
+		ArrayList<String> isOnline = hashmap.get(split[0]);
+		if ((value != null) && (isOnline != null) && (isOnline.size() % 2 != 0)) {
+			arraylist = hashmap.get(split[2]);
+			String str = "";
+			for (String line : arraylist) { str = str + line + ":"; }
+	        if (arraylist.size() != 0) {
+	        	return "ok:" + split[2] + ":true:" + ((arraylist.size()+1)/2) + str;
+	        } else return "ok:" + split[2] + ":false:" + ((arraylist.size())/2) + str;
+		} else return "error:notlogged";
+	}
+	
+	private String listavailable(ArrayList<String> arraylist) {	
+		String str = "";
+		for (Object key : hashmap.keySet()) {
+			arraylist = hashmap.get(key);
+			if (arraylist.size() % 2 != 0) {
+				str = str + key + ":";
+			}
+		}
+		str = str + "\n";
+		return str;
+	}
+	
+	private String listabsen(ArrayList<String> arraylist) {	
+		System.out.print("ok:");
+		String str = "";
+		for (Object key : hashmap.keySet()) {
+			arraylist = hashmap.get(key);
+			if (arraylist.size() % 2 == 0) {
+				str = str + key + ":";
+			}
+		}	
+		str = str + "\n";
+		return str;
+	}	
+	
 	public String executeCommands(String[] split, EchoServer echoServer) throws IllegalArgumentException, IOException {	  
 		ArrayList<String> arraylist = new ArrayList<String>();
 		
 		SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyyMMdd'T'HH'_'mm'_'ss.SSSZ");
-		Date dateNow = new Date(0 );
+		Date dateNow = new Date(0);
 		
-		if ("login".equals(split[1])){
-		    if (hashmap.get(split[0]) == null) {
-		    	arraylist = new ArrayList<String>();
-			    arraylist.add(dateFormat.format(dateNow));
-			    hashmap.put(split[0], arraylist);
-		    } else {
-				arraylist = hashmap.get(split[0]);
-
-		    	arraylist.add(dateFormat.format(dateNow));
-			    hashmap.put(split[0], arraylist);
-		    }
-		    return "ok";
-		} else
-		if ("logout".equals(split[1])){	
-			arraylist = hashmap.get(split[0]);
-
-			if (hashmap.get(split[0]) == null) {
-				return "error:notlogged";
-			} else if (arraylist.size() % 2 != 0) {
-			    arraylist.add(dateFormat.format(dateNow));
-			    hashmap.put(split[0], arraylist);
-			    return "ok";
-			} return "error:notlogged";
-
-		} else
-		
-		if("info".equals(split[1])) {
-			Object value = hashmap.get(split[2]);
-			ArrayList<String> isOnline = hashmap.get(split[0]);
-			if ((value != null) && (isOnline != null) && (isOnline.size() % 2 != 0)) {
-				arraylist = hashmap.get(split[2]);
-				String str = "";
-				for (String line : arraylist) { str = str + line + ":"; }
-		        if (arraylist.size() != 0) {
-		        	return "ok:" + split[2] + ":true:" + ((arraylist.size()+1)/2) + str;
-		        } else return "ok:" + split[2] + ":false:" + ((arraylist.size())/2) + str;
-			} else return "error:notlogged";
-		} else
-		
-		if ("listavailable".equals(split[1])){
-			for (Object key : hashmap.keySet()) {
-				arraylist = hashmap.get(key);
-				if (arraylist.size() % 2 != 0) {
-					System.out.println(key);
-				}
-			}
-		} else 
-		if ("listabsen".equals(split[1])){
-			System.out.print("ok:");
-			for (Object key : hashmap.keySet()) {
-				arraylist = hashmap.get(key);
-				if (arraylist.size() % 2 == 0) {
-					System.out.print(key);
-					System.out.print(":");
-				}
-			}	
-			System.out.println();
-		} else
-		
-		if ("shutdown".equals(split[1])) {
+		if ("login".equals(split[1]))
+			return login(split, arraylist, dateNow, dateFormat);
+		else if ("logout".equals(split[1]))	
+			return logout(split, arraylist, dateNow, dateFormat);
+		else if("info".equals(split[1])) 
+			return info(split, arraylist);
+		else if ("listavailable".equals(split[1]))
+			return listavailable(arraylist);
+		else if ("listabsen".equals(split[1]))
+			return listabsen(arraylist);
+		else if ("shutdown".equals(split[1])) 
 			echoServer.stopServer();
-		} else return "error:unknowncommand";
-		return "ok";
+		else return "error:unknowncommand";
+//		return "ok";
+		return null;
 	}
 }

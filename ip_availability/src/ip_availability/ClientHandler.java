afaf2deec98723508d1175f4d0ac6009c445f820
kamena <kamena.dacheva@gmail.com>
2015-11-17 00:15:05 +0200

    Create new Java Project + EchoServer + login, logout and shutdown command.

---
+++ ip_availability/src/ip_availability/ClientHandler.java
@@ -0,0 +1,86 @@
+package ip_availability;
+
+//import java.io.PrintStream;
+//import java.net.Socket;
+//import java.text.SimpleDateFormat;
+//import java.util.*;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable {
+	final static HashMap<String, ArrayList<String>> hashmap = 
+	        new HashMap<String, ArrayList<String>>();
+	private final Socket socket;
+
+	private final EchoServer echoServer;
+
+	public ClientHandler(EchoServer echoServer, Socket socket) {
+		this.socket = socket;
+		this.echoServer = echoServer;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			final PrintStream out = 
+				new PrintStream(socket.getOutputStream());
+			final Scanner scanner =
+				new Scanner(socket.getInputStream());
+			while (scanner.hasNextLine()) {
+				final String line = scanner.nextLine();
+				final String[] split = line.split(":");	  
+				ArrayList<String> arraylist = new ArrayList<String>();
+				
+				SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyyMMdd'T'HH'_'mm'_'ss.SSSZ");
+				Date dateNow = new Date(0 );
+				
+				if ("login".equals(split[1])){
+				    if (hashmap.get(split[0]) == null) {
+				    	arraylist = new ArrayList<String>();
+					    arraylist.add(dateFormat.format(dateNow));
+					    hashmap.put(split[0], arraylist);
+				    } else {
+						arraylist = hashmap.get(split[0]);
+
+				    	arraylist.add(dateFormat.format(dateNow));
+					    hashmap.put(split[0], arraylist);
+				    }
+				    out.println("ok");
+				} else
+				if ("logout".equals(split[1])){	
+					arraylist = hashmap.get(split[0]);
+
+					if (hashmap.get(split[0]) == null) {
+						out.println("error:notlogged:logout:1");
+					} else if (arraylist.size() % 2 != 0) {
+					    arraylist.add(dateFormat.format(dateNow));
+					    hashmap.put(split[0], arraylist);
+					    out.println("ok");
+					} else out.println("error:notlogged:logout:2");
+
+				} else
+				
+				if ("shutdown".equals(split[1])) {
+					echoServer.stopServer();
+				} else out.println("error:unknowncommand:shutdown");
+			}
+			scanner.close();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			echoServer.onClientStopped(this);
+		}
+	}
+	
+	public void stopClient() throws IOException {
+		socket.close();
+	}
+}
---
+++ ip_availability/src/ip_availability/EchoServer.java
@@ -0,0 +1,77 @@
+package ip_availability;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EchoServer {
+	private final int port;
+	private boolean running;
+	private final List<ClientHandler> clients =
+		Collections.synchronizedList(
+			new LinkedList<ClientHandler>());
+	private ServerSocket serverSocket;
+
+	public EchoServer(int port) {
+		this.port = port;
+	}
+
+	public void startServer() throws IOException {
+		final ServerSocket localServerSocket = createServerSocket();
+		
+		// to synchronize access to the variable 'running'
+		while(isRunning()) {
+			final Socket socket;
+			try {
+				socket = localServerSocket.accept();
+			} catch (SocketException e) {
+				if (!localServerSocket.isClosed()) {
+					throw e;
+				}
+				break;
+			}
+
+			final ClientHandler client =
+				new ClientHandler(this, socket);
+			clients.add(client);
+			new Thread(client).start();
+		}
+	}
+
+	private synchronized ServerSocket createServerSocket() throws IOException {
+		if (running) {
+			throw new IllegalStateException("Already running");
+		}
+
+		running = true;
+		serverSocket = new ServerSocket(port);
+		return serverSocket;
+	}
+	
+	public synchronized boolean isRunning() {
+		return running;
+	}
+
+	public synchronized void stopServer() throws IOException {
+		if (!running) {
+			throw new IllegalStateException("Not running");
+		}
+
+		running = false;
+		
+		serverSocket.close();
+		serverSocket = null;
+		
+		for (ClientHandler next : clients) {
+			next.stopClient();
+		}
+	}
+
+	public void onClientStopped(ClientHandler clientHandler) {
+		clients.remove(clientHandler);
+	}
+}
